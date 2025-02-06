5c977672113df1c410375f6985f569b8348b1fb4
Chaejin Lim <jin.chaejin.lim@adcapsule.co.kr>
2025-02-06 17:55:06 +0900

    WIP

--- lib/firebase_config.dart
+++ lib/firebase_config.dart
@@ -1,6 +1,5 @@
-// lib/firebase_config.dart
+import 'package:app52switch/firebase_options.dart';
 
-import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 //import 'dart:io';
@@ -13,39 +12,11 @@ class FirebaseConfig {
   // Method to load Firebase configuration dynamically
   static Future<void> loadFirebaseConfig() async {
     // Initialize Firebase with the dynamically generated DefaultFirebaseOptions
-    // await Firebase.initializeApp(
-    //     options: DefaultFirebaseOptions.currentPlatform);
-    FirebaseOptions firebaseOptions;
-
-    print("firebase_options.dart not found, falling back to environment variables.");
-
-    // Fallback to environment variables if `firebase_options.dart` is missing
-    if (Platform.isAndroid) {
-      firebaseOptions = FirebaseOptions(
-        apiKey: const String.fromEnvironment('FIREBASE_ANDROID_API_KEY'),
-        appId: const String.fromEnvironment('FIREBASE_ANDROID_APP_ID'),
-        messagingSenderId: const String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID'),
-        projectId: const String.fromEnvironment('FIREBASE_PROJECT_ID'),
-        storageBucket: const String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
-      );
-    } else if (Platform.isIOS) {
-      firebaseOptions = FirebaseOptions(
-        apiKey: const String.fromEnvironment('FIREBASE_IOS_API_KEY'),
-        appId: const String.fromEnvironment('FIREBASE_IOS_APP_ID'),
-        messagingSenderId: const String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID'),
-        projectId: const String.fromEnvironment('FIREBASE_PROJECT_ID'),
-        storageBucket: const String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
-        iosBundleId: const String.fromEnvironment('FIREBASE_IOS_BUNDLE_ID'),
-      );
-    } else {
-      throw UnsupportedError("Unsupported platform");
-    }
-      LoggerConfig()
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+    LoggerConfig()
       .logger
-      .i("Firebase initialized with injected values.");
-    
-
-    await Firebase.initializeApp(options: firebaseOptions);
+      .i("Firebase initialized with default file.");
 
     if (EnvConfig.useEmulator) {
       // Initialize Firebase Emulator if required
